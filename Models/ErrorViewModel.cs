a81aa547d242e734a21e9248bc17c5fca03f26f4
Vuong702 <vuong180900@gmail.com>
2023-10-23 22:44:35 +0700

    fist commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NVV_PTPMQL_BT.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
